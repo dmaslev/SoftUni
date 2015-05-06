c5060c7d4f4c1ea492c4b458f0766a6dfcc04f5e
dmaslev <detelinmaslev@gmail.com>
2015-05-07 01:49:05 +0300

    Revert "Catch the bitsp"
    This reverts commit 89bf89961cae5b541b951ac3603593e54db06d60.

--- 05.InputOutputConsole/17.CatchTheBits/Program.cs
+++ 05.InputOutputConsole/17.CatchTheBits/Program.cs
@@ -7,42 +7,32 @@ class BitsUp
     {
         int n = int.Parse(Console.ReadLine());
         int step = int.Parse(Console.ReadLine());
-        int position = 0;
+        int index = 0;
         int result = 0;
-        int counter = 0;
-
+        
         for (int i = 0; i < n; i++)
         {
-
             int number = int.Parse(Console.ReadLine());
 
             for (int j = 7; j >= 0; j--)
             {
                 int a = 1;
-                if ((position % step == 1) || (step == 1 && position > 0))
+                if ((index % step == 1) || (step == 1 && index > 0))
                 {
                     result = result << 1;
                     int bit = number & (1 << a);
                     result = result | bit;
                     a++;
-                    counter++;
-
+                    
                     
                 }
-                ++position;
+                ++index;
 
-                
             }
 
-           
-        }
-        while (counter % 8  > 0)
-        {
-            result = result << 1;
-            counter++;
+
         }
         Console.WriteLine(Convert.ToString(result, 2));
-        Console.WriteLine(result);
 
 
 
