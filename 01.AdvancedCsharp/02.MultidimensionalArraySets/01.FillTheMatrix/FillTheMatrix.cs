c710846af0717b5dfb3d45f7de9b22b78d4c2dea
dmaslev <detelinmaslev@gmail.com>
2015-05-07 02:38:14 +0300

    02.Methods-homework

---
+++ 01.AdvancedCsharp/02.MultidimensionalArraySets/01.FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FillTheMatrix
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = 1 + i * n + j;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("{0} ", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
---
+++ 01.AdvancedCsharp/03.Methods/Test/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Test
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Row(n);
+    }
+
+    private static void Row(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            PrintRow(i);
+        }
+        for (int a = n-1; a >=1; a--)
+        {
+            PrintRow(a);
+        }
+    }
+
+    private static void PrindSecond(int a, int n)
+    {
+        for (int b = n; b > 1; b--)
+        {
+            Console.Write("{0} ", n- b + 1);
+        }
+        Console.WriteLine();
+    }
+
+    private static void PrintRow(int i)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Console.Write("{0} ", j + 1);
+        }
+        Console.WriteLine();
+    }
+}
+
