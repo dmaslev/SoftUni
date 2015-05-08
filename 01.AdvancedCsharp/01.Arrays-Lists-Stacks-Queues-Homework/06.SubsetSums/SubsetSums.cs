92b0205733c8a811af3f9b45a65a50643a7e7ecf
dmaslev <detelinmaslev@gmail.com>
2015-05-08 14:17:37 +0300

    Added Subsetsum

--- 01.AdvancedCsharp/01.Arrays-Lists-Stacks-Queues-Homework/06.SubsetSums/SubsetSums.cs
+++ 01.AdvancedCsharp/01.Arrays-Lists-Stacks-Queues-Homework/06.SubsetSums/SubsetSums.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _06.SubsetSums
+class SortedSubsets
 {
-    class SubsetSums
+    static void Main()
     {
-        static void Main(string[] args)
+        int sum = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        int[] numbers = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Distinct().ToArray();
+        bool resultFound = false;
+        List<int> currentSubset = new List<int>();
+        List<List<int>> result = new List<List<int>>();
+        int combinations = (int)Math.Pow(2, numbers.Length);
+
+        for (int i = 1; i < combinations; i++)
         {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (((i >> j) & 1) == 1)
+                {
+                    currentSubset.Add(numbers[j]);
+                }
+            }
+            if (currentSubset.Sum() == sum)
+            {
+                result.Insert(0, new List<int>(currentSubset));
+                resultFound = true;
+            }
+            currentSubset = new List<int>();
         }
+        if (!resultFound)
+        {
+            Console.WriteLine("No matching subsets.");
+            return;
+        }        
+        foreach (var item in result)
+        {
+            Console.WriteLine("{0} = {1}", string.Join(" + ", item), sum);
+        }
+
     }
 }
