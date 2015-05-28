edac7070b2b242caba6dba609912170add30fc56
dmaslev <detelinmaslev@gmail.com>
2015-05-29 00:34:53 +0300

    exam preparation

---
+++ 01.AdvancedCsharp/ExamPreparation-21-05-2015/02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {   
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++)
+        {
+            sb.AppendFormat(" {0}", Console.ReadLine());
+        }
+
+        string arrowPatten = @"(>>>----->>)|(>>----->)|(>----->)";
+        Regex arrowMatcher = new Regex(arrowPatten);
+        var arrows = arrowMatcher.Matches(sb.ToString());
+        int largeCount = 0;
+        int mediumCount = 0;
+        int smallCount = 0;
+
+        foreach (Match arrow in arrows)
+        {
+            if (!string.IsNullOrEmpty(arrow.Groups[1].Value))
+            {
+                largeCount++;
+            }
+            else if (!string.IsNullOrEmpty(arrow.Groups[2].Value))
+            {
+                mediumCount++;
+            }
+            else
+            {
+                smallCount++;
+            }
+        }
+
+        string numberAsString = "" + smallCount + mediumCount + largeCount;
+        string binaryNumber = Convert.ToString(Convert.ToInt32( numberAsString), 2);
+        string reversed = new string(binaryNumber.Reverse().ToArray());
+        string resultNumber = binaryNumber + reversed;
+        int result = Convert.ToInt32(resultNumber, 2);
+        Console.WriteLine(result);
+    }
+}
+
---
+++ 01.AdvancedCsharp/ExamPreparation-21-05-2015/03/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<char[]> matrix = new List<char[]>();
+
+        const string symbols = "<>^v";
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "END")
+            {
+                break;
+            }
+            matrix.Add(line.ToCharArray());
+        }
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            for (int col = 0; col < matrix[row].Length; col++)
+            {
+                int currentRow;
+                int currentCol;
+
+                switch (matrix[row][col])
+                {
+                    case '<':
+                        currentRow = row;
+                        currentCol = col - 1;
+                        while (currentCol >= 0 && !symbols.Contains(matrix[currentRow][currentCol].ToString()))
+                        {
+                            matrix[currentRow][currentCol] = ' ';
+                            currentCol--;
+                        }
+                        break;
+                    case '>':
+                        currentRow = row;
+                        currentCol = col + 1;
+                        while (currentCol < matrix[currentRow].Length && !symbols.Contains(matrix[currentRow][currentCol].ToString()))
+                        {
+                            matrix[currentRow][currentCol] = ' ';
+                            currentCol++;
+                        }
+                        break;
+                    case '^':
+                        currentRow = row - 1;
+                        currentCol = col;
+                        while (currentRow >= 0 && !symbols.Contains(matrix[currentRow][currentCol].ToString()))
+                        {
+                            matrix[currentRow][currentCol] = ' ';
+                            currentRow--;
+                        }
+                        break;
+                    case 'v':
+                        currentRow = row + 1;
+                        currentCol = col;
+                        while (currentRow < matrix.Count && !symbols.Contains(matrix[currentRow][currentCol].ToString()))
+                        {
+                            matrix[currentRow][currentCol] = ' ';
+                            currentRow++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+        PrintMatrix(matrix);
+
+    }
+    private static void PrintMatrix(List<char[]> matrix)
+    {
+        for (int row = 0; row < matrix.Count; row++)
+        {
+            Console.WriteLine("<p>{0}</p>", SecurityElement.Escape( new string(matrix[row])));
+        }
+    }
+}
+
