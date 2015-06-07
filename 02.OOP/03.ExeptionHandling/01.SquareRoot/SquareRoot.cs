c1d0950c34c36ac713d3f516d267990dd410e60a
dmaslev <detelinmaslev@gmail.com>
2015-06-08 01:53:02 +0300

    zxcv

---
+++ 02.OOP/03.ExeptionHandling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            //uint a = uint.Parse(Console.ReadLine());
+            try
+            {
+                uint number = uint.Parse(Console.ReadLine());
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine(squareRoot);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+            
+        }
+    }
+}
---
+++ 02.OOP/03.ExeptionHandling/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int start = 0;
+
+            for (int i = 0; i < 10; )
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    ReadNumber(start, number);
+                    start = number;
+                    i++;
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Try again");
+                    //throw;
+                }
+            }
+        }
+
+        private static void ReadNumber(int start, int number)
+        {
+            if (number <= start || number >= 101)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
