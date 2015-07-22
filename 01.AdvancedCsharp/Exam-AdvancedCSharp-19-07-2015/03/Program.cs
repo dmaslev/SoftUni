ae570832eff8b1dd7fced316503696f49da21020
Detelin Maslev <detelinmaslev@gmail.com>
2015-07-22 03:09:43 +0300

    advanced C# exam

---
+++ 01.AdvancedCsharp/Exam-AdvancedCSharp-19-07-2015/03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = long.MaxValue ^ 0;
+            Console.WriteLine(a);
+        }
+    }
+}
