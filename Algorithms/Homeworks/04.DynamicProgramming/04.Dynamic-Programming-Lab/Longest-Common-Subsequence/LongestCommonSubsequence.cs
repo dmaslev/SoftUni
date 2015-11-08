233d2f30ab0cce6ef988db844f1df7a6a4a1f323
dmaslev <detelinmaslev@gmail.com>
2015-11-08 02:19:47 +0200

    .

---
+++ Algorithms/Homeworks/04.DynamicProgramming/04.Dynamic-Programming-Lab/Longest-Common-Subsequence/LongestCommonSubsequence.cs
@@ -0,0 +1,78 @@
+﻿namespace Longest_Common_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LongestCommonSubsequence
+    {
+        public static void Main()
+        {
+            var firstStr = "tree";
+            var secondStr = "team";
+
+            var lcs = FindLongestCommonSubsequence(firstStr, secondStr);
+
+            Console.WriteLine("Longest common subsequence:");
+            Console.WriteLine("  first  = {0}", firstStr);
+            Console.WriteLine("  second = {0}", secondStr);
+            Console.WriteLine("  lcs    = {0}", lcs);
+        }
+
+        public static string FindLongestCommonSubsequence(string firstStr, string secondStr)
+        {
+            int firstLength = firstStr.Length + 1;
+            int secondLength = secondStr.Length + 1;
+            var lcs = new int[firstLength, secondLength];
+
+            for (int i = 1; i < firstLength; i++)
+            {
+                for (int j = 1; j < secondLength; j++)
+                {
+                    if (firstStr[i - 1] == secondStr[j - 1])
+                    {
+                        lcs[i, j] = lcs[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        lcs[i, j] = Math.Max(lcs[i - 1, j], lcs[i, j - 1]);
+                    }
+                }
+            }
+
+            List<char> sequence = RetrieveLCS(firstStr, secondStr, lcs);
+            sequence.Reverse();
+            return new string(sequence.ToArray());
+        }
+
+        private static List<char> RetrieveLCS(string firstStr, string secondStr, int[,] lcs)
+        {
+            List<char> lcsWord = new List<char>();
+
+            for (int i = lcs.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = lcs.GetLength(1) - 1; j >= 0; j--)
+                {
+                    while (i > 0 && j > 0)
+                    {
+                        if (firstStr[i - 1] == secondStr[j - 1])
+                        {
+                            lcsWord.Add(firstStr[i - 1]);
+                            j--;
+                            i--;
+                        }
+                        else if (lcs[i, j] == lcs[i - 1, j])
+                        {
+                            i--;
+                        }
+                        else
+                        {
+                            j--;
+                        }
+                    }
+                }
+            }
+
+            return lcsWord;
+        }
+    }
+}
---
+++ Algorithms/Homeworks/05.Graph-Algorithms/05.Graph-Algorithms-Lab/Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+
+    private HashSet<string> visitedNodes;
+
+    private LinkedList<string> sortedNodes;
+
+    private HashSet<string> cycleNodes;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    public IEnumerable<string> TopSort()
+    {
+        this.visitedNodes = new HashSet<string>();
+        this.sortedNodes = new LinkedList<string>();
+        this.cycleNodes = new HashSet<string>();
+
+        foreach (var node in graph.Keys)
+        {
+            TopSortDFS(node);
+        }
+
+        return this.sortedNodes;
+
+        //var predecessorsCount = new Dictionary<string, int>();
+        //foreach (var node in this.graph)
+        //{
+        //    if (!predecessorsCount.ContainsKey(node.Key))
+        //    {
+        //        predecessorsCount[node.Key] = 0;
+        //    }
+        //    foreach (var childNode in node.Value)
+        //    {
+        //        if (!predecessorsCount.ContainsKey(childNode))
+        //        {
+        //            predecessorsCount[childNode] = 0;
+        //        }
+
+        //        predecessorsCount[childNode]++;
+        //    }
+        //}
+
+        //var removeNodes = new List<string>();
+        //while (true)
+        //{
+        //    string nodeToRemove = graph.Keys.FirstOrDefault(n => predecessorsCount[n] == 0);
+        //    if (nodeToRemove == null)
+        //    {
+        //        break;
+        //    }
+
+        //    foreach (var childNode in graph[nodeToRemove])
+        //    {
+        //        predecessorsCount[childNode]--;
+        //    }
+        //    graph.Remove(nodeToRemove);
+        //    removeNodes.Add(nodeToRemove);
+
+        //}
+
+        //if (graph.Count > 0)
+        //{
+        //    throw new InvalidOperationException("A cycle detected in the graph");
+        //}
+
+        //return removeNodes;
+    }
+
+    private void TopSortDFS(string node)
+    {
+        if (this.cycleNodes.Contains(node))
+        {
+            throw new InvalidOperationException("A cycle detected in the graph!");
+        }
+
+        if (!this.visitedNodes.Contains(node))
+        {
+            this.visitedNodes.Add(node);
+            this.cycleNodes.Add(node);
+
+            if (graph.ContainsKey(node))
+            {
+                foreach (var child in graph[node])
+                {
+                    TopSortDFS(child);
+                }
+            }
+
+            this.cycleNodes.Remove(node);
+            this.sortedNodes.AddFirst(node);
+        }
+    }
+}
