d4e7d3c12ab0742a958d8666ddb566fbf8bbc1fe
dmaslev <detelinmaslev@gmail.com>
2015-12-05 03:10:09 +0200

    needles and words added

---
+++ Algorithms/Homeworks/03. Sorting-and-Searching-Algorithms-Lab-Skeleton/07.Words/Words.cs
@@ -0,0 +1,70 @@
+﻿namespace _07.Words
+{
+    using System;
+
+    class Words
+    {
+        private static int counter = 0;
+
+        static void Main()
+        {
+            var input = Console.ReadLine().ToCharArray();
+            Array.Sort(input);
+            PermuteRep(input, 0, input.Length-1);
+            Console.WriteLine(counter);
+        }
+
+        private static void PermuteRep(char[] numbers, int start, int end)
+        {
+            if (IsValidSolution(numbers))
+            {
+                counter++;
+            }
+
+            for (int left = end - 1; left >= start; left--)
+            {
+                for (int right = left + 1; right <= end; right++)
+                {
+                    if (numbers[left] != numbers[right])
+                    {
+                        Swap(ref numbers[left], ref numbers[right]);
+                        PermuteRep(numbers, left + 1, end);
+                    }
+                }
+
+                var firstElement = numbers[left];
+                for (int i = left; i <= end - 1; i++)
+                {
+                    numbers[i] = numbers[i + 1];
+                }
+
+                numbers[end] = firstElement;
+            }
+        }
+
+        private static bool IsValidSolution(char[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i] == array[i+1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void Swap(ref char p1, ref char p2)
+        {
+            if (p1 == p2)
+            {
+                return;
+            }
+
+            p1 ^= p2;
+            p2 ^= p1;
+            p1 ^= p2;
+        }
+    }
+}
---
+++ Algorithms/Homeworks/03. Sorting-and-Searching-Algorithms-Lab-Skeleton/08.Needles/Needles.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.Needles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Needles
+    {
+
+        static void Main()
+        {
+            string bullshit = Console.ReadLine();
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] result = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = FindIndex(array, numbers[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int FindIndex(int[] array, int v)
+        {
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                if (v > array[i] && array[i] != 0)
+                {
+                    return i+ 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
